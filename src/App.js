8f13a5c29db9a3fc354fa95333356d0856707913
ChristieLaf <133935258+ChristieLaf@users.noreply.github.com>
2023-07-25 13:58:20 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import SheCodesPluslogo from "./SheCodesPluslogo.png";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {" "}
+        <img src={SheCodesPluslogo} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
