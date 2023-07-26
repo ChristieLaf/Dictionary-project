d23880506f76ffabed347721d06546b3defefa44
ChristieLaf <133935258+ChristieLaf@users.noreply.github.com>
2023-07-26 09:33:04 -0400

    Added search engine

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import "./App.css";
 import SheCodesPluslogo from "./SheCodesPluslogo.png";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        {" "}
-        <img src={SheCodesPluslogo} className="App-logo" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          {" "}
+          <img src={SheCodesPluslogo} className="App-logo" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          {" "}
+          <small>Coded by Christie</small>{" "}
+        </footer>
+      </div>
     </div>
   );
 }
