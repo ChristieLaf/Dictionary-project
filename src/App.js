93e3331087296e484c51f50f4c4b3f35cb7855e7
ChristieLaf <133935258+ChristieLaf@users.noreply.github.com>
2023-08-05 01:47:18 -0400

    Modifications to Design

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import SheCodesPluslogo from "./SheCodesPluslogo.png";
+import ChristieWebLogo from "./ChristieWebLogo.PNG";
 import Dictionary from "./Dictionary";
 
 export default function App() {
@@ -8,14 +8,20 @@ export default function App() {
       <div className="container">
         <header className="App-header">
           {" "}
-          <img src={SheCodesPluslogo} className="App-logo" alt="logo" />
+          <img
+            src={ChristieWebLogo}
+            className="App-logo"
+            alt="logo"
+            width={200}
+            height={50}
+          />
         </header>
         <main>
           <Dictionary defaultKeyword="sunrise" />
         </main>
         <footer className="App-footer">
           {" "}
-          <small>Coded by Christie</small>{" "}
+          <small>Coded by Christie Laferriere</small>{" "}
         </footer>
       </div>
     </div>
