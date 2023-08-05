7da78497387b321d36df43fc1508a466196cab2f
ChristieLaf <133935258+ChristieLaf@users.noreply.github.com>
2023-08-04 21:51:55 -0400

    Added photos to be displayed

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,32 +1,37 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
-  function handleResponse(response) {
-    console.log(response.data);
-    console.log(response.data[0].meanings[0].definitions[0].definition);
-    console.log(
-      response.data.meanings?.[0]?.definitions?.[0]?.synonyms ||
-        "No synonyms available"
-    );
+  function handleDictionResponse(response) {
     setResults(response.data[0]);
   }
 
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
+  }
+
   function search() {
-    // documentation: https://dictionaryapi.dev/
-    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
+    // documentation: https://dictionaryapi.dev/e
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    axios.get(apiUrl).then(handleDictionResponse);
+
+    let pexelsApiKey =
+      "PIKY3nA3hBElXcSxwppnKGkDZnh3X4bujULNVA3CgPdAzLAh5TASUOZX";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
 
   function handleSubmit(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword} definition`);
     search();
   }
 
@@ -47,16 +52,16 @@ export default function Dictionary(props) {
           <form onSubmit={handleSubmit}>
             <input
               type="search"
-              autoFocus={true}
               onChange={handleKeywordChange}
               defaultValue={props.defaultKeyword}
             />
           </form>
           <div className="hint">
-            Suggested words: sunset, wine, yoga. plant ....
+            suggested words: sunset, wine, yoga, plant...
           </div>
         </section>
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
